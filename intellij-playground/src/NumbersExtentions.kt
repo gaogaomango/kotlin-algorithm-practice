32b12eadf28c0b3a024fec221bdf601b68ab4e3a
gaogaomango <gaogaomango@gmail.com>
2019-04-21 19:20:56 +0900

    find median val in two arrays

---
+++ intellij-playground/src/NumbersExtentions.kt
@@ -0,0 +1,13 @@
+object NumbersExtentions {
+    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+        val sortedNums = nums1.plus(nums2).sorted()
+        val mid = sortedNums.size / 2
+
+        return if(sortedNums.size % 2 == 0) {
+            (sortedNums[mid] + sortedNums[mid - 1]).toDouble() / 2
+        } else {
+            sortedNums[mid].toDouble()
+        }
+
+    }
+}
